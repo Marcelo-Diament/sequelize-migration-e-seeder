e67f931a4dcf2f1aaa3419e81b947434ccd6bcfc
Marcelo-Diament <marcelodiament@gmail.com>
2021-11-03 22:00:28 -0300

    bugfix: ajustando migrations e seeders

--- server/database/seeders/20211102172917-demo-users.js
+++ server/database/seeders/20211102172917-demo-users.js
@@ -24,7 +24,7 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date()
       }
-    ])
+    ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
---
+++ server/database/seeders/20211102173809-demo-statuses.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Statuses', [
+      {
+        title: '01- A Desenvolver',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        title: '02 - Em Desenvolvimento',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        title: '03 - A Validar',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        title: '04 - Em Validação',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        title: '05 - Validado',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        title: '06 - A Ajustar',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        title: '07 - Finalizado',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Statuses', null, {});
+  }
+};
